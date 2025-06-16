5925d3f6e866ee8748f99365c08df3d85d136d96
MIKU <157250727+MikuXVDEV@users.noreply.github.com>
2025-06-16 13:48:36 +0300

    Добавлена поддержка цифр

--- vosk_tts/g2p.py
+++ vosk_tts/g2p.py
@@ -1,60 +1,86 @@
 # -- coding: utf-8 --
 
+import re
+from num2words import num2words  # <-- Добавлено
+
+
+
 # Converts an accented vocabulary to dictionary, for example
 #
 # абстракцион+истов
 # абстр+акцию
 # абстр+акция
-# 
+#
 # абстракционистов a0 b s t r a0 k c i0 o0 nj i1 s t o0 v
 # абстракцию a0 b s t r a1 k c i0 j u0
 # абстракция a0 b s t r a1 k c i0 j a0
 #
 
-softletters=set(u"яёюиье")
-startsyl=set(u"#ъьаяоёуюэеиы-")
+def fix_number_commas(text):
+    text = re.sub(r'\b(миллиард[а-я]*)\b', r'\1,', text)
+    text = re.sub(r'\b(миллион[а-я]*)\b', r'\1,', text)
+    text = re.sub(r'\b(тысяч[а-я]*)\b', r'\1,', text)
+    return text
+
+softletters = set(u"яёюиье")
+startsyl = set(u"#ъьаяоёуюэеиы-")
 others = set(["#", "+", "-", u"ь", u"ъ"])
 
 softhard_cons = {
-    u"б" : u"b",
-    u"в" : u"v",
-    u"г" : u"g",
-    u"Г" : u"g",
-    u"д" : u"d",
-    u"з" : u"z",
-    u"к" : u"k",
-    u"л" : u"l",
-    u"м" : u"m",
-    u"н" : u"n",
-    u"п" : u"p",
-    u"р" : u"r",
-    u"с" : u"s",
-    u"т" : u"t",
-    u"ф" : u"f",
-    u"х" : u"h"
+    u"б": u"b",
+    u"в": u"v",
+    u"г": u"g",
+    u"Г": u"g",
+    u"д": u"d",
+    u"з": u"z",
+    u"к": u"k",
+    u"л": u"l",
+    u"м": u"m",
+    u"н": u"n",
+    u"п": u"p",
+    u"р": u"r",
+    u"с": u"s",
+    u"т": u"t",
+    u"ф": u"f",
+    u"х": u"h"
 }
 
 other_cons = {
-    u"ж" : u"zh",
-    u"ц" : u"c",
-    u"ч" : u"ch",
-    u"ш" : u"sh",
-    u"щ" : u"sch",
-    u"й" : u"j"
+    u"ж": u"zh",
+    u"ц": u"c",
+    u"ч": u"ch",
+    u"ш": u"sh",
+    u"щ": u"sch",
+    u"й": u"j"
 }
 
 vowels = {
-    u"а" : u"a",
-    u"я" : u"a",
-    u"у" : u"u",
-    u"ю" : u"u",
-    u"о" : u"o",
-    u"ё" : u"o",
-    u"э" : u"e",
-    u"е" : u"e",
-    u"и" : u"i",
-    u"ы" : u"y",
-}                                
+    u"а": u"a",
+    u"я": u"a",
+    u"у": u"u",
+    u"ю": u"u",
+    u"о": u"o",
+    u"ё": u"o",
+    u"э": u"e",
+    u"е": u"e",
+    u"и": u"i",
+    u"ы": u"y",
+}
+
+def replace_digits_with_words(text: str) -> str:
+    """Заменяет числа на слова (только если вся строка состоит из одного числа)."""
+    def repl(m):
+        try:
+            return num2words(int(m.group()), lang='ru')
+        except:
+            return m.group()
+        
+
+
+    fix_numbers = fix_number_commas(re.sub(r'\b\d+\b', repl, text))
+
+
+    return fix_numbers
 
 def pallatize(phones):
     for i, phone in enumerate(phones[:-1]):
@@ -78,12 +104,13 @@ def convert_vowels(phones):
         else:
             new_phones.append(phone[0])
         prev = phone[0]
-
     return new_phones
 
 def convert(stressword):
-    phones = ("#" + stressword + "#")
+    stressword = replace_digits_with_words(stressword)  # <-- Цифры в слова
 
+    phones = ("#" + stressword + "#")
+    print(phones)
 
     # Assign stress marks
     stress_phones = []
@@ -103,5 +130,7 @@ def convert(stressword):
 
     # Filter
     phones = [x for x in phones if x not in others]
+    print(" ".join(phones)
+)
 
     return " ".join(phones)
